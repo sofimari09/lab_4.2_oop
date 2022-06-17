b8bbd43bcf90343cddd2041141917219cb6c39a2
Lenovo <Desofiyamariya@gmail.com>
2022-06-17 16:09:37 +0300

    Add project files.

---
+++ GEOprog.cpp
@@ -0,0 +1,10 @@
+#include "GEOprog.h"
+#include <iostream>
+
+using namespace std;
+
+double GEOprog::element(int j) const
+{
+	return geta() * pow(getr(), j);
+
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Element.h"
+#include "../ARprog.cpp"
+#include "../GEOprog.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Element* el = new ARprog(0, 5);
+			Assert::AreEqual((int)el->element(3), 15);
+
+		}
+	};
+}
